fd0ed1e9c91d39286e4acc1275f9a4c013e226d9
Alex Vallejo <vallejo.alex@gmail.com>
2021-02-02 10:07:36 -0800

    Initial commit

---
+++ ToLowerBenchmark/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace ToLowerBenchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<ToLowerBenchmark>();
+        }
+    }
+
+    public class ToLowerBenchmark
+    {
+        private readonly string Data;
+
+        public ToLowerBenchmark()
+        {
+            var rand = new Random();
+            var sb = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.Append(rand.Next('A', 'z' + 1));
+            }
+
+            this.Data = sb.ToString();
+        }
+
+        [Benchmark]
+        public string ToLower() => this.Data.ToLower();
+
+        [Benchmark]
+        public string ToLowerInvariant() => this.Data.ToLowerInvariant();
+
+    }
+}
